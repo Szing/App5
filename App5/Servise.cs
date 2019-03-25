8da22fc4cf43fe59e932507069f0a22b7792b883
Szing <1319167111@qq.com>
2019-03-25 19:55:49 +0800

    add a service

---
+++ App5/Servise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5
+{
+    class Servise
+    {
+        public void dos() { }
+
+    }
+}
